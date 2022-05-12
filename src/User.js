b946d9d744eb4615255525969e23e33e949b744f
Dale Hoover <dalehoover94@gmail.com>
2022-05-11 22:59:52 -0500

    First Commit

---
+++ src/User.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Link,
+  NavLink,
+  Route,
+  Switch,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+import UserPosts from "./UserPosts";
+import UserProfile from "./UserProfile";
+
+export const User = ({ users = [] }) => {
+  const { userId } = useParams();
+  const {path, url}= useRouteMatch();
+ 
+
+  if (!userId) {
+    throw new Error("No URL parameter for userId");
+  }
+
+  const user = users.find((user) => `${user.id}` === userId);
+
+  if (user) {
+    return (
+      <section>
+        <Link to="/"> &lt;- Users</Link>
+        <div>
+          <h2>{user.name}</h2>
+          <ul>
+            <li>
+              <NavLink to={url} data-testid="user-profile">
+                Profile
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to={`${url}/posts`} data-testid="user-posts">
+                Posts
+              </NavLink>
+            </li>
+          </ul>
+          <Route exact path={path}>
+          <UserProfile user={user} />
+          </Route>
+          
+          <Route path= {`${path}/posts`}>
+          <UserPosts posts={user.posts} />
+          </Route>
+        </div>
+      </section>
+    );
+  }
+  return <p>User not found</p>;
+};
+
+export default User;
